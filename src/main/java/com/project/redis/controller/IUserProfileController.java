a5d183f77542689f4b84c60b14e45bc2b1268bee
RufanNiyazli <developerx73@gmail.com>
2026-01-08 22:15:13 +0400

    feat:Controller

---
+++ src/main/java/com/project/redis/controller/IUserProfileController.java
@@ -0,0 +1,4 @@
+package com.project.redis.controller;
+
+public interface IUserProfileController {
+}
---
+++ src/main/java/com/project/redis/controller/impl/UserProfileControllerImpl.java
@@ -0,0 +1,4 @@
+package com.project.redis.controller.impl;
+
+public class UserProfileControllerImpl {
+}
